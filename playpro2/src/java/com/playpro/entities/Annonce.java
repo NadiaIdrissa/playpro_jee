6b2e5d0b8b175f68ea7fcc85659b9edc57a4825c
Kennedy Kalomba <kennedymukendi@gmail.com>
2019-10-07 15:04:53 -0400

    ajout des classes

---
+++ playpro2/src/java/com/playpro/entities/Annonce.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.playpro.entities;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author toute
+ */
+public class Annonce {
+
+    private int idAnnonce;
+    private Entraineur createur;
+    private String titre;
+    private String message;
+    private LocalDate dateCreation;
+}
