a36d5e92d6f70e7529df74d0cd8b66f7f23d7b81
Kennedy Kalomba <kennedymukendi@gmail.com>
2019-10-15 22:47:27 -0400

    ajout javascript et optimisation modal créée par younes

--- playpro2/src/java/com/playpro/mvc2/controleurs/SignupAction.java
+++ playpro2/src/java/com/playpro/mvc2/controleurs/SignupAction.java
@@ -5,6 +5,9 @@
  */
 package com.playpro.mvc2.controleurs;
 
+import com.playpro.entities.Entraineur;
+import com.playpro.entities.Joueur;
+import com.playpro.entities.Niveau;
 import com.playpro.utils.PasswordHash;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
@@ -24,31 +27,52 @@ public class SignupAction extends AbstractAction {
         String email = (String) request.getParameter("email");
         String password = (String) request.getParameter("password");
         String confirm = (String) request.getParameter("confirm_password");
-        
-        if (nom == null || prenom == null || email == null || password == null 
-                || confirm == null){
+        String niveau = (String) request.getParameter("niveau");
+        String sport = (String) request.getParameter("sport");
+
+        Joueur j;
+        Entraineur e;
+
+        for (Niveau n : Niveau.values()) {
+
+            System.out.println(n);
+        }
+
+        if (nom == null || prenom == null || email == null || password == null
+                || confirm == null) {
             System.out.println("Infos inexistantes");
             return "signup";
-        }
-        else{
+        } else {
             System.out.println("Infos OK !");
-            
+
             //Hachage du password (classe PasswordHash)
             try {
                 String mdpH = PasswordHash.createHash(password);
+
+                System.out.println("Mot de passe: " + password);
+
                 boolean isMdpOk = PasswordHash.validatePassword(password, mdpH);
-                
-                System.out.println("Les mots de passes sont égaux : "+ isMdpOk);
-                
+
+                System.out.println("Mot de passe hashé: " + mdpH);
+
+                System.out.println("Les mots de passes sont égaux : " + isMdpOk);
+
+                if (niveau != null) {
+                    j = new Joueur();
+                }
+
+                if (sport != null) {
+                    e = new Entraineur();
+                }
+
             } catch (NoSuchAlgorithmException ex) {
                 Logger.getLogger(SignupAction.class.getName()).log(Level.SEVERE, null, ex);
             } catch (InvalidKeySpecException ex) {
                 Logger.getLogger(SignupAction.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            
             return "login";
-        }    
+        }
     }
 
 }
