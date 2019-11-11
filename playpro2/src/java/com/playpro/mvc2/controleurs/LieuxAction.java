61cca36b5b50dd1a193eac3f401819e501a607a4
Kennedy Kalomba <kennedymukendi@gmail.com>
2019-11-11 12:23:16 -0500

    Lieux

--- playpro2/src/java/com/playpro/mvc2/controleurs/LieuxAction.java
+++ playpro2/src/java/com/playpro/mvc2/controleurs/LieuxAction.java
@@ -9,14 +9,22 @@ import com.playpro.daos.LieuxDAO;
 import com.playpro.entities.Lieux;
 import com.playpro.factories.ObjectFactory;
 import com.playpro.services.LieuxServices;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.Part;
 
 /**
  *
  * @author nadym
  */
 public class LieuxAction extends AbstractAction {
+    
+    private static final String UPLOAD_DIR = "static/images/lieux";
 
     @Override
     public String execute() {
@@ -29,18 +37,45 @@ public class LieuxAction extends AbstractAction {
         String numero = request.getParameter("numero");
         String cp = request.getParameter("code_postal");
         String infos = request.getParameter("infos");
+        
+        String image1 = request.getParameter("image1");
+        
         Lieux s = new Lieux();
 
         if (ville == null || rue == null || pays == null || cp == null) {
             
         } else {
-            nom = "Nom";
-            rue = "Rue";
-            numero = "1212";
-            ville = "Dorval";
-            pays = "Canada";
-            cp = "H1H2H2";
-            infos = "Accueil";
+            String applicationPath = request.getServletContext().getRealPath("");
+            // constructs path of the directory to save uploaded file
+            String uploadFilePath = applicationPath + File.separator + UPLOAD_DIR;
+
+            // creates the save directory if it does not exists
+            File fileSaveDir = new File(uploadFilePath);
+            if (!fileSaveDir.exists()) {
+                fileSaveDir.mkdirs();
+            }
+            System.out.println("Upload File Directory=" + fileSaveDir.getAbsolutePath());
+
+            try {
+                //Get all the parts from request and write it to the file on server
+                for (Part part : request.getParts()) {
+                    String fileName = getFileName(part);
+                    if (!fileName.equals("")) {
+                        part.write(uploadFilePath + File.separator + fileName);
+                        image1 = fileName;
+                        System.out.println("FILE NAME : "+fileName);
+                    }
+                }
+                request.setAttribute("message", "File uploaded successfully!");
+            } catch (IOException ex) {
+                Logger.getLogger(UploadAction.class.getName()).log(Level.SEVERE, null, ex);
+                request.setAttribute("message", "File NOT uploaded successfully!");
+            } catch (ServletException ex) {
+                Logger.getLogger(UploadAction.class.getName()).log(Level.SEVERE, null, ex);
+                request.setAttribute("message", "File NOT uploaded successfully!");
+            }
+            
+            
             s = ObjectFactory.getNewLieu();
             s.setNom(nom);
             s.setRue(rue);
@@ -49,6 +84,7 @@ public class LieuxAction extends AbstractAction {
             s.setNumero(numero);
             s.setCode_postal(cp);
             s.setInfos(infos);
+            s.setImage1(image1);
 
             LieuxServices.creerLieux(s);
         }
@@ -69,6 +105,18 @@ public class LieuxAction extends AbstractAction {
         request.setAttribute("lieux", liste);
         request.setAttribute("AfficherLieux", true);
         return "portail";
+    }
+    
+    private String getFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        System.out.println("content-disposition header= " + contentDisp);
+        String[] tokens = contentDisp.split(";");
+        for (String token : tokens) {
+            if (token.trim().startsWith("filename")) {
+                return token.substring(token.indexOf("=") + 2, token.length() - 1);
+            }
+        }
+        return "";
     }
 
 }
