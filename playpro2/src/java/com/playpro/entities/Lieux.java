79efae22d0a6778ef2bf0697bfc1031a4f8d8948
Nadia_Idrissa <nmoussaidrissa@dept-info.crosemont.quebec>
2019-11-11 12:23:19 -0500

    modal lieu

--- playpro2/src/java/com/playpro/entities/Lieux.java
+++ playpro2/src/java/com/playpro/entities/Lieux.java
@@ -19,6 +19,9 @@ public class Lieux {
     private String ville;
     private String pays;
     private String infos;
+    private String image1;
+    private String image2;
+    private String image3;
 
     public Lieux() {
     }
@@ -90,6 +93,30 @@ public class Lieux {
     @Override
     public String toString() {
         return "Lieux{" + "id_lieu=" + id_lieu + ", nom=" + nom + ", numero=" + numero + ", rue=" + rue + ", code_postal=" + code_postal + ", ville=" + ville + ", pays=" + pays + ", infos=" + infos + '}';
+    }
+
+    public String getImage1() {
+        return image1;
+    }
+
+    public String getImage2() {
+        return image2;
+    }
+
+    public String getImage3() {
+        return image3;
+    }
+
+    public void setImage1(String image1) {
+        this.image1 = image1;
+    }
+
+    public void setImage2(String image2) {
+        this.image2 = image2;
+    }
+
+    public void setImage3(String image3) {
+        this.image3 = image3;
     }
     
     
