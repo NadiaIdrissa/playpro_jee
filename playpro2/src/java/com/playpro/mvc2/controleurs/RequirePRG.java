3f46d00dc839cecb2b9ad62f135ba1c593a251cb
Kennedy Kalomba <kennedymukendi@gmail.com>
2019-11-20 18:35:10 -0500

    ajout file uploader dans profil

---
+++ playpro2/src/java/com/playpro/mvc2/controleurs/RequirePRG.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.playpro.mvc2.controleurs;
+
+/**
+ *
+ * @author moumene
+ */
+public interface RequirePRG {   
+    /*Interface vide ou interface de marquage (tagging interface)*/
+}
