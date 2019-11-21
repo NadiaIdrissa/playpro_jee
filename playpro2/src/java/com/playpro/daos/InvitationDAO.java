22d6656868bbe11829ca6f6e1775c89b56a7cb2b
Salpy Garabedian <salpy_1995@hotmail.com>
2019-11-21 00:39:17 -0500

    classe invitation et invitationDAO

---
+++ playpro2/src/java/com/playpro/daos/InvitationDAO.java
@@ -0,0 +1,319 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.playpro.daos;
+
+
+import com.playpro.entities.Invitation;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Salpy
+ */
+public class InvitationDAO extends DAO<Invitation>{
+    
+    public InvitationDAO (){
+       super();
+    }
+
+    @Override
+    public boolean create(Invitation x) {
+        String req = "INSERT INTO invitation (`ID_EXPEDITEUR`, `ID_DESTINATAIRE`, `ID_EQUIPE`, `DATE_ENVOI`) VALUES (?,?,?,?)";
+
+        PreparedStatement paramStm = null;
+        try {
+            paramStm = cnx.prepareStatement(req);
+            paramStm.setString(1, x.getIdExpediteur());
+            paramStm.setString(2, x.getIdDestinataire());
+            paramStm.setString(3, x.getIdEquipe());
+            paramStm.setTimestamp(4, x.getDateEnvoi());
+            
+            int nbLignesAffectees= paramStm.executeUpdate();
+            if (nbLignesAffectees>0){
+                paramStm.close();
+                return true;
+            }
+            
+            return false;
+        }    
+        catch (SQLException exp){
+            Logger.getLogger(InvitationDAO.class.getName()).log(Level.SEVERE, null, exp);
+        }
+        finally {
+            try {
+                if (paramStm!=null)
+                    paramStm.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(InvitationDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
+
+    
+    public Invitation read(int id) {
+        try {
+            return this.read(id);
+        }
+        catch (NumberFormatException e){
+            return null;
+        }
+    }
+
+   
+    public Invitation read(String id) {
+        
+        String req = "SELECT * FROM invitation WHERE 'ID_EXPEDITEUR'= ?";
+        
+        PreparedStatement paramStm = null;
+        try {
+
+            paramStm = cnx.prepareStatement(req);
+
+            paramStm.setString(1, id);
+
+            ResultSet resultat = paramStm.executeQuery();
+
+            
+            if(resultat.next()){
+                Invitation invite = new Invitation();
+                invite.setIdExpediteur(resultat.getString("ID_EXPEDITEUR"));
+                invite.setIdDestinataire(resultat.getString("ID_DESTINATAIRE"));
+                invite.setDate(resultat.getTimestamp("DATE"));
+                invite.setIdEquipe(resultat.getString("ID_PROJET"));
+                
+                resultat.close();
+                paramStm.close();
+                return invite;
+            }
+
+            resultat.close();
+            paramStm.close();
+            return null;
+
+        }
+        catch (SQLException exp) {
+        }
+        finally {
+            try{
+                if (paramStm!=null)
+                    paramStm.close();
+            }
+            catch (SQLException exp) {
+            }
+             catch (Exception e) {
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean update(Invitation x) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Invitation x) {
+        String req = "DELETE FROM invitation WHERE `ID_EXPEDITEUR` = ? AND `ID_DESTINATAIRE` = ? AND `ID_PROJET` = ? ";
+            PreparedStatement paramStm = null;
+            try {
+
+                paramStm = cnx.prepareStatement(req);
+                paramStm.setString(1, x.getIdExpediteur());
+                paramStm.setString(2, x.getIdDestinataire());
+                paramStm.setString(3, x.getIdEquipe());
+
+                int nbLignesAffectees= paramStm.executeUpdate();
+
+                if (nbLignesAffectees>0) {
+                    paramStm.close();
+                    return true;
+                }
+
+                return false;
+            }
+            catch (SQLException exp) {
+            }
+            finally {
+                try {
+                    if (paramStm!=null)
+                        paramStm.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(InvitationDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+            return false;
+    }
+
+    @Override
+    public List<Invitation> findAll() {
+        List<Invitation> liste = new LinkedList<>();
+        try {
+            Statement stm = cnx.createStatement();
+            ResultSet r = stm.executeQuery("SELECT * FROM invitation");
+            while (r.next()) {
+                Invitation invite = new Invitation();
+                invite.setIdExpediteur(r.getString("ID_EXPEDITEUR"));
+                invite.setIdDestinataire(r.getString("ID_DESTINATAIRE"));
+                invite.setIdEquipe(r.getString("ID_PROJET"));
+                invite.setDate(r.getTimestamp("DATE"));
+                liste.add(invite);
+            }
+            Collections.reverse(liste);
+            r.close();
+            stm.close();
+        }
+        catch (SQLException exp) {
+            Logger.getLogger(InvitationDAO.class.getName()).log(Level.SEVERE, null, exp);
+        }        
+        return liste;
+    }
+    
+    public List<Invitation> findByIdDestinataire(String id) {
+       String req = "SELECT * FROM invitation WHERE `ID_DESTINATAIRE` = ? ";
+        
+        List<Invitation> liste = new LinkedList<>();
+        try {
+            PreparedStatement stm = cnx.prepareStatement(req);
+            stm.setString(1, id);
+            ResultSet r = stm.executeQuery();            
+            while (r.next()) {
+                Invitation invite = new Invitation();
+                invite.setIdExpediteur(r.getString("ID_EXPEDITEUR"));
+                invite.setIdDestinataire(r.getString("ID_DESTINATAIRE"));
+                invite.setIdEquipe(r.getString("ID_PROJET"));
+                invite.setDate(r.getTimestamp("DATE"));
+                liste.add(invite);
+            }
+           
+            Collections.reverse(liste);
+            r.close();
+            stm.close();
+        }
+        catch (SQLException exp) {
+        }
+        return liste;
+    }
+    
+       public List<Invitation> findByIdExpediteur(String id) {
+       String req = "SELECT * FROM invitation WHERE `ID_EXPEDITEUR` = ? ";
+        
+        List<Invitation> liste = new LinkedList<>();
+        try {
+            PreparedStatement stm = cnx.prepareStatement(req);
+            stm.setString(1, id);
+            ResultSet r = stm.executeQuery();            
+            while (r.next()) {
+                Invitation invite = new Invitation();
+                invite.setIdExpediteur(r.getString("ID_EXPEDITEUR"));
+                invite.setIdDestinataire(r.getString("ID_DESTINATAIRE"));
+                invite.setIdEquipe(r.getString("ID_PROJET"));
+                invite.setDate(r.getTimestamp("DATE"));
+                liste.add(invite);
+            }
+          
+            Collections.reverse(liste);
+            r.close();
+            stm.close();
+        }
+        catch (SQLException exp) {
+        }
+        return liste;
+    }
+    
+    public Invitation findByIdProjet(String id) {
+        String req = "SELECT * FROM invitation WHERE `ID_PROJET` = ?";
+
+        PreparedStatement paramStm = null;
+        try {
+            paramStm = cnx.prepareStatement(req);
+
+            paramStm.setString(1, id);
+
+            ResultSet resultat = paramStm.executeQuery();
+
+            
+            if(resultat.next()){
+
+                Invitation invite = new Invitation();
+                invite.setIdExpediteur(resultat.getString("ID_EXPEDITEUR"));
+                invite.setIdDestinataire(resultat.getString("ID_DESTINATAIRE"));
+                invite.setIdEquipe(resultat.getString("ID_PROJET"));
+                invite.setDate(resultat.getTimestamp("DATE"));
+                resultat.close();
+                paramStm.close();
+                return invite;
+            }
+
+            resultat.close();
+            paramStm.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(InvitationDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public Invitation findByDateEnvoi(Timestamp date) {
+        String req = "SELECT * FROM invitation WHERE `DATE` = ?";
+
+        PreparedStatement paramStm = null;
+        try {
+            paramStm = cnx.prepareStatement(req);
+
+            paramStm.setTimestamp(1, date);
+
+            ResultSet resultat = paramStm.executeQuery();
+
+            
+            if(resultat.next()){
+
+                Invitation invite = new Invitation();
+                invite.setIdExpediteur(resultat.getString("ID_EXPEDITEUR"));
+                invite.setIdDestinataire(resultat.getString("ID_DESTINATAIRE"));
+                invite.setIdEquipe(resultat.getString("ID_PROJET"));
+                invite.setDate(resultat.getTimestamp("DATE"));
+                resultat.close();
+                paramStm.close();
+                return invite;
+            }
+
+            resultat.close();
+            paramStm.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(InvitationDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public Invitation findById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Invitation findById(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean UpdateStatus(Invitation x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
--- playpro2/src/java/com/playpro/entities/Invitation.java
+++ playpro2/src/java/com/playpro/entities/Invitation.java
@@ -5,17 +5,75 @@
  */
 package com.playpro.entities;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  *
- * @author dd
+ * @author Salpy
  */
 public class Invitation {
-    private String id;
-    private String nomEquipe;
-    private String idMembre;
-    private LocalDate dateEnvoi;
-    private String statut;
+    private String idExpediteur;
+    private String idDestinataire;
+    private String idEquipe;
+    private Timestamp dateEnvoi;
+    
+    
+    public Invitation() {
+        this.idExpediteur = "";
+        this.idDestinataire = "";
+        this.idEquipe = "";
+        this.dateEnvoi = Timestamp.valueOf(LocalDateTime.now());
+        
+    }
+    
+    public Invitation(String idExpediteur, String idDestinataire, String idEquipe, Timestamp dateEnvoi) {
+        this.idExpediteur = idExpediteur;
+        this.idDestinataire = idDestinataire;
+        this.idEquipe = idEquipe;
+        this.dateEnvoi = dateEnvoi;
+    }
+    
+    public Invitation(String idExpediteur, String idDestinataire, String idEquipe) {
+        this.idExpediteur = idExpediteur;
+        this.idDestinataire = idDestinataire;
+        this.idEquipe = idEquipe;
+        this.dateEnvoi = Timestamp.valueOf(LocalDateTime.now());
+    }
+    
+    public String getIdExpediteur() {
+        return idExpediteur;
+    }
+
+    public void setIdExpediteur(String idExpediteur) {
+        this.idExpediteur = idExpediteur;
+    }
+
+    public String getIdDestinataire() {
+        return idDestinataire;
+    }
+
+    public void setIdDestinataire(String idDestinataire) {
+        this.idDestinataire = idDestinataire;
+    }
+
+    public String getIdEquipe() {
+        return idEquipe;
+    }
+    
+    public void setIdEquipe(String idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    public Timestamp getDateEnvoi() {
+        return dateEnvoi;
+    }
+
+    public void setDate(Timestamp dateEnvoi) {
+        this.dateEnvoi = dateEnvoi;
+    }
+    
+
     
 }
