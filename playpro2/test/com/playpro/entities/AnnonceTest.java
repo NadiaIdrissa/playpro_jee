4c70603204a812a96b2d0d7b5c54f1d27205e981
acarbonellalfonso <acarbonellalfonso@dept-info.crosemont.quebec>
2019-11-27 20:57:36 -0500

    clone

--- playpro2/test/com/playpro/entities/AnnonceTest.java
+++ playpro2/test/com/playpro/entities/AnnonceTest.java
@@ -57,13 +57,7 @@ public class AnnonceTest {
      */
     @Test
     public void testGetIdCreateur() {
-        System.out.println("getIdCreateur");
-        Annonce instance = new Annonce();
-        String expResult = "";
-        String result = instance.getIdCreateur();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
