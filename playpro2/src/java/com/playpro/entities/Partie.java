e0e3e6f041fb79353a9c03214aa55f477ed6f15f
acarbonellalfonso <acarbonellalfonso@dept-info.crosemont.quebec>
2019-11-29 00:05:50 -0500

    classe Partie

--- playpro2/src/java/com/playpro/entities/Partie.java
+++ playpro2/src/java/com/playpro/entities/Partie.java
@@ -13,10 +13,81 @@ import java.time.LocalTime;
  * @author toute
  */
 public class Partie {
-    private int idPartie;
+
+    private String idPartie;
     private LocalDate datePartie;
     private LocalTime heurePArtie;
     private Equipe equipe1;
     private Equipe equipe2;
     private LieuSport lieuSportPartie;
+    private Sport sport;
+    private String score;
+    Equipe eq = new Equipe();
+
+    public Partie() {
+    }
+
+    public String getId_Partie() {
+        return idPartie;
+    }
+
+    public void setId_capitaine(String idPartie) {
+        this.idPartie = idPartie;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+
+    public Equipe getEquipe1() {
+        return equipe1;
+    }
+
+    public void setEquipe1(Equipe equipe1) {
+        this.equipe1 = equipe1;
+    }
+
+    public Equipe getEquipe2() {
+        return equipe2;
+    }
+
+    public void setEquipe2(Equipe equipe2) {
+        this.equipe2 = equipe2;
+    }
+
+    public LocalDate getDatePartie() {
+        return datePartie;
+    }
+
+    public void setDatePartie(LocalDate datePartie) {
+        this.datePartie = datePartie;
+    }
+
+    public LocalTime getHeurePartie() {
+        return heurePArtie;
+    }
+
+    public void setHeurePartie(LocalTime heurePartie) {
+        this.heurePArtie = heurePartie;
+    }
+
+    public LieuSport getLieuSportPartie() {
+        return lieuSportPartie;
+    }
+
+    public void setLieuSportPartie(LieuSport lieu) {
+        this.lieuSportPartie =lieu;
+    }
 }
